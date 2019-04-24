832f783fbbdc1344714e319912c5173be1521b32
Rafael <rafaelbaez.diseno@gmail.com>
2019-04-24 00:30:21 -0500

    varmix

--- wp-content/themes/rafaelbgtema/index.php
+++ wp-content/themes/rafaelbgtema/index.php
@@ -2,14 +2,10 @@
 get_header();
 ?>
 <h2>yay</h2>
-<h2>asdasdasd</h2>
 <h2>asdasdasasdasd</h2>
 <h2>asdasdasd</h2>
+<h2>asdsd</h2>
 <h2>asdasdasd</h2>
-<h2>asdasdasd</h2>
-<h2>asdasdasd</h2>
-<h2>asdasdasd</h2>
-<h2>asdasdasdasdasd</h2>
 
 <?php
 get_footer();

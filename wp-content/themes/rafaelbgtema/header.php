8d848bbd893e6e927f7eb72cf3afc5c8e1421ce2
Rafael <rafaelbaez.diseno@gmail.com>
2019-06-13 21:25:33 -0500

    responsive del menu terminado

--- wp-content/themes/rafaelbgtema/header.php
+++ wp-content/themes/rafaelbgtema/header.php
@@ -20,36 +20,37 @@
 <body <?php body_class()?> >
 
 	<div class="menuprincipal">
-		<img src="<?php echo get_theme_file_uri('img/logo.svg') ?>" class="menuprincipal__logo">
-		<ul class="menuprincipal__lista">
-			<li class="menuprincipal__vinculo <?php if ( is_front_page() ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php if (is_front_page() ) { echo '#portada'; } else { echo esc_url(site_url()); } ?>">Inicio</a></li>
-			<li class="menuprincipal__vinculo"><a href="<?php if( is_front_page() ) { echo '#sobremi'; } else { echo esc_url(site_url()).'#sobremi'; } ?>">Sobre mi</a></li>
-			<li class="menuprincipal__vinculo <?php if ( is_page('servicios') ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php echo esc_url(site_url('/servicios')) ?>">Servicios</a></li>
-			<li class="menuprincipal__vinculo <?php if ( is_page('portafolio') OR  is_singular('proyectos') ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php echo esc_url(site_url('/portafolio')) ?>">Portafolio</a></li>
-			<li class="menuprincipal__vinculo <?php if ( is_page('contacto') ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php if( is_page('servicios') OR is_page('portafolio') OR is_front_page() ) { echo '#contacto'; } else { echo esc_url(site_url('/contacto')); } ?>">Contacto</a></li>
-		</ul>
+		<div class="menuprincipal__wrapperoverflow">
+			<img src="<?php echo get_theme_file_uri('img/logo.svg') ?>" class="menuprincipal__logo">
+			<ul class="menuprincipal__lista">
+				<li class="menuprincipal__vinculo <?php if ( is_front_page() ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php if (is_front_page() ) { echo '#portada'; } else { echo esc_url(site_url()); } ?>">Inicio</a></li>
+				<li class="menuprincipal__vinculo"><a href="<?php if( is_front_page() ) { echo '#sobremi'; } else { echo esc_url(site_url()).'#sobremi'; } ?>">Sobre mi</a></li>
+				<li class="menuprincipal__vinculo <?php if ( is_page('servicios') ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php echo esc_url(site_url('/servicios')) ?>">Servicios</a></li>
+				<li class="menuprincipal__vinculo <?php if ( is_page('portafolio') OR  is_singular('proyectos') ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php echo esc_url(site_url('/portafolio')) ?>">Portafolio</a></li>
+				<li class="menuprincipal__vinculo <?php if ( is_page('contacto') ) { echo 'menuprincipal__vinculo--actual'; } ?>"><a href="<?php if( is_page('servicios') OR is_page('portafolio') OR is_front_page() ) { echo '#contacto'; } else { echo esc_url(site_url('/contacto')); } ?>">Contacto</a></li>
+			</ul>
 
-		<div class="menuprincipal__footer">
-			<div class="menuprincipal__social">
-				<a href="https://www.facebook.com/rafaelbg.dyd" target="_blank">
-					<i class="fab fa-facebook menuprincipal__social__item"></i>
-				</a>
-				<a href="https://www.instagram.com/rafael.bg.dyd/" target="_blank">
-					<i class="fab fa-instagram menuprincipal__social__item"></i>
-				</a>
-				<a href="http://m.me/rafaelbg.dyd">
-					<i class="fab fa-facebook-messenger menuprincipal__social__item"></i>
-				</a>
-				<a href="https://api.whatsapp.com/send?phone=525554646711&text=%C2%A1Hola,%20Rafael!%20Quiero%20contarte%20de%20mi%20proyecto.">
-					<i class="fab fa-whatsapp menuprincipal__social__item"></i>
-				</a>
+			<div class="menuprincipal__footer">
+				<div class="menuprincipal__social">
+					<a href="https://www.facebook.com/rafaelbg.dyd" target="_blank">
+						<i class="fab fa-facebook menuprincipal__social__item"></i>
+					</a>
+					<a href="https://www.instagram.com/rafael.bg.dyd/" target="_blank">
+						<i class="fab fa-instagram menuprincipal__social__item"></i>
+					</a>
+					<a href="http://m.me/rafaelbg.dyd">
+						<i class="fab fa-facebook-messenger menuprincipal__social__item"></i>
+					</a>
+					<a href="https://api.whatsapp.com/send?phone=525554646711&text=%C2%A1Hola,%20Rafael!%20Quiero%20contarte%20de%20mi%20proyecto.">
+						<i class="fab fa-whatsapp menuprincipal__social__item"></i>
+					</a>
+				</div>
+
+				<a href="mailto:hola@rafaelbg.com" class="menuprincipal__footer__item">hola@rafaelbg.com</a>
+				<span class="menuprincipal__footer__separador">|</span>
+				<a href="tel:5554646711" class="menuprincipal__footer__item">55 5464 6711</a>
 			</div>
-
-			<a href="mailto:hola@rafaelbg.com" class="menuprincipal__footer__item">hola@rafaelbg.com</a>
-			<span class="menuprincipal__footer__separador">|</span>
-			<a href="tel:5554646711" class="menuprincipal__footer__item">55 5464 6711</a>
 		</div>
-		
 			<div class="barranavegacion">
 				<a href="<?php esc_url(site_url()) ?>">
 					<img src="<?php echo get_theme_file_uri('img/logo.svg') ?>" class="barranavegacion__logo">
